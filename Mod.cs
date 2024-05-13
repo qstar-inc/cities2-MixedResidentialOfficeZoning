7dbff699ef382a15587309957ac33ee82ab96c43
Qoushik <qstar.inc@gmail.com>
2024-05-14 02:51:14 +0600

    update 2.0.1

--- Mod.cs
+++ Mod.cs
@@ -1,11 +1,17 @@
 ﻿using Colossal.IO.AssetDatabase;
 //using Colossal.Logging;
+using Colossal.PSI.Common;
+using Colossal.PSI.Environment;
 using Colossal.UI;
 using Game.Modding;
 using Game.PSI;
 using Game.SceneFlow;
+using Game.UI.Menu;
 using Game;
 using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using Unity.Entities;
 
 
 namespace MixedResidentialOfficeZoning
@@ -17,6 +23,7 @@ namespace MixedResidentialOfficeZoning
         public static readonly string openModPage = "https://mods.paradoxplaza.com/mods/79067/Windows";
         public static MixedResidentialOfficeZoningSetting Setting;
         public static string uiHostName = "starq-mixed-office";
+        public static NotificationUISystem _notificationUISystem;
         public void OnLoad(UpdateSystem updateSystem)
         {
             //log.Info(nameof(OnLoad));
@@ -24,15 +31,25 @@ namespace MixedResidentialOfficeZoning
             //if (GameManager.instance.modManager.TryGetExecutableAsset(this, out var asset))
             //log.Info($"Current mod asset at {asset.path}");
             GameManager.instance.modManager.TryGetExecutableAsset(this, out var asset);
+            _notificationUISystem = World.DefaultGameObjectInjectionWorld.GetOrCreateSystemManaged<NotificationUISystem>();
             UIManager.defaultUISystem.AddHostLocation(uiHostName, Path.Combine(Path.GetDirectoryName(asset.path), "thumbs"), false);
             Setting = new MixedResidentialOfficeZoningSetting(this);
+            Setting.RegisterInOptionsUI();
+            GameManager.instance.localizationManager.AddSource("en-US", new LocaleEN(Setting));
             AssetDatabase.global.LoadSettings(nameof(MixedResidentialOfficeZoning), Setting, new MixedResidentialOfficeZoningSetting(this));
             Setting.DummySetting = false;
-            showChangeNotification();
-            
         }
 
-        protected void showChangeNotification()
+        public void OnCreate()
+        {
+            ShowChangeNotification();
+            if (Setting.ProcessedThumbnails == false)
+            {
+                ProcessThumbnail();
+            }
+        }
+
+        protected void ShowChangeNotification()
         {
             if (Setting != null && Setting.HasReadChangeNotice == false)
             {
@@ -43,8 +60,8 @@ namespace MixedResidentialOfficeZoning
                     onClicked: OpenModPage
                     );
             }
-
         }
+
         public static void OpenModPage()
         {
             System.Diagnostics.Process.Start(openModPage);
@@ -52,9 +69,109 @@ namespace MixedResidentialOfficeZoning
             Setting.HasReadChangeNotice = true;
         }
 
+        public static void ProcessThumbnail()
+        {
+            string modFolder = EnvPath.kCacheDataPath + "/Mods/mods_subscribed/";
+            string ailCustomFolder = EnvPath.kUserDataPath + "/ModsData/AssetIconLibrary/CustomThumbnails";
+
+            var thumbNotification = _notificationUISystem.AddOrUpdateNotification("starq-mixed-office-thumbnail-process",
+                title: "Processing Thumbnails (Mixed Residential Office Zoning)",
+                text: "Starting system...",
+                progressState: ProgressState.Indeterminate,
+                progress: 0);
+            if (Directory.GetDirectories(modFolder, "79634_*").Length > 0)
+            {
+                if (!CheckExistingFiles(ailCustomFolder, thumbNotification))
+                {
+                    FindAndCopyFiles(modFolder, ailCustomFolder, thumbNotification);
+                }
+            } else
+            {
+                thumbNotification.text = "Asset Icon Library not found, cancelling...";
+                thumbNotification.progressState = ProgressState.Complete;
+                thumbNotification.progress = (4 / 4 * 100);
+            }
+        }
+
+        protected static bool CheckExistingFiles(string ailCustomFolder, NotificationUISystem.NotificationInfo thumbNotification = null)
+        {
+            Directory.CreateDirectory(ailCustomFolder);
+            string[] existingFiles = Directory.GetFiles(ailCustomFolder, "*.png");
+            int count = 0;
+
+            if (existingFiles.Length > 0)
+            {
+                count = existingFiles.Count(file =>
+                {
+                    Match match = Regex.Match(Path.GetFileName(file), @"(([EUNA]{2})_MixedOffice([LeftRight\d]{2,7})_L([\d])_([\dx]{3}).png)");
+                    return match.Success;
+                });
+            }
+
+            if (thumbNotification != null && count == 330)
+            {
+                thumbNotification.text = "Thumbnails already exists; cancelling...";
+                thumbNotification.progressState = ProgressState.Complete;
+                thumbNotification.progress = (4 / 4 * 100);
+                _notificationUISystem.RemoveNotification("starq-mixed-office-thumbnail-process");
+            }
+            return count == 330;
+
+        }
+
+        protected static void FindAndCopyFiles(string modFolder, string ailCustomFolder, NotificationUISystem.NotificationInfo thumbNotification)
+        {
+            string[] directories = Directory.GetDirectories(modFolder, "79634_*");
+
+            if (directories.Length > 0)
+            {
+                thumbNotification.text = "Asset Icon Library found...";
+                thumbNotification.progressState = ProgressState.Progressing;
+                thumbNotification.progress = (2 / 4 * 100);
+            }
+            foreach (string directory in directories)
+            {
+
+                string thumbdirectory = directory + "/Thumbnails";
+                string[] files = Directory.GetFiles(thumbdirectory, "*.png");
+                if (files.Length > 0)
+                {
+                    thumbNotification.text = $"Configuring thumbnails...";
+                    thumbNotification.progressState = ProgressState.Progressing;
+                    thumbNotification.progress = (3 / 4 * 100);
+                }
+                foreach (string file in files)
+                {
+                    Match match = Regex.Match(Path.GetFileName(file), @"(([EUNA]{2})_Mixed([LeftRight\d]{2,7})_L([\d])_([\dx]{3})\.png)");
+
+
+                    if (match.Success)
+                    {
+                        string newFileName = match.Groups[2].Value + "_MixedOffice" + match.Groups[3].Value + "_L" + match.Groups[4].Value + "_" + match.Groups[5].Value + ".png";
+                        string ailCustomFolderFilePath = Path.Combine(ailCustomFolder, newFileName);
+                        File.Copy(file, ailCustomFolderFilePath, true);
+                    }
+                }
+            }
+
+            if (CheckExistingFiles(ailCustomFolder))
+            {
+                thumbNotification.text = "Thumbnail processing completed...";
+                thumbNotification.progressState = ProgressState.Complete;
+                thumbNotification.progress = (4 / 4 * 100);
+                Setting.ProcessedThumbnails = true;
+                _notificationUISystem.RemoveNotification("starq-mixed-office-thumbnail-process");
+            }
+        }
+
         public void OnDispose()
         {
             UIManager.defaultUISystem.RemoveHostLocation(uiHostName);
+            if (Setting != null)
+            {
+                Setting.UnregisterInOptionsUI();
+                Setting = null;
+            }
         }
     }
 }
--- Settings.cs
+++ Settings.cs
@@ -1,12 +1,16 @@
-﻿using Colossal.IO.AssetDatabase;
+﻿using Colossal;
+using Colossal.IO.AssetDatabase;
 using Game.Modding;
 using Game.Settings;
+using System.Collections.Generic;
 
 namespace MixedResidentialOfficeZoning
 {
     [FileLocation("ModsSettings\\" + nameof(MixedResidentialOfficeZoning))]
     public class MixedResidentialOfficeZoningSetting : ModSetting
     {
+        public const string sectionMain = "Main";
+        public const string actions = "Actions";
 
         public MixedResidentialOfficeZoningSetting(IMod mod) : base(mod)
         {
@@ -14,18 +18,59 @@ namespace MixedResidentialOfficeZoning
         }
 
         [SettingsUIHidden]
+        public bool ProcessedThumbnails { get; set; }
+
+        [SettingsUIHidden]
         public bool HasReadChangeNotice { get; set; }
+
+        [SettingsUIHidden]
         public bool DummySetting { get; set; }
+
+
+        [SettingsUIButton]
+        [SettingsUISection(sectionMain, actions)]
+        public bool RedoThumbnail
+        {
+            set { Mod.ProcessThumbnail(); }
+        }
 
         public override void SetDefaults()
         {
             DummySetting = true;
             HasReadChangeNotice = false;
+            ProcessedThumbnails = false;
         }
 
         public override void Apply()
         {
             base.Apply();
+        }
+    }
+
+    public class LocaleEN : IDictionarySource
+    {
+        private readonly MixedResidentialOfficeZoningSetting m_Setting;
+        public LocaleEN(MixedResidentialOfficeZoningSetting setting)
+        {
+            m_Setting = setting;
+        }
+        public IEnumerable<KeyValuePair<string, string>> ReadEntries(IList<IDictionaryEntryError> errors, Dictionary<string, int> indexCounts)
+        {
+            return new Dictionary<string, string>
+            {
+                { m_Setting.GetSettingsLocaleID(), "Mixed Residential Office Zoning" },
+                { m_Setting.GetOptionTabLocaleID(MixedResidentialOfficeZoningSetting.sectionMain), "Main" },
+                { m_Setting.GetOptionGroupLocaleID(MixedResidentialOfficeZoningSetting.actions), "Actions" },
+
+                { m_Setting.GetOptionLabelLocaleID(nameof(MixedResidentialOfficeZoningSetting.RedoThumbnail)), "Reset Thumbnail Cache [powered by Asset Icon Library]" },
+                { m_Setting.GetOptionDescLocaleID(nameof(MixedResidentialOfficeZoningSetting.RedoThumbnail)), $"This button will redo the thumbnails from the Asset Icon Library to work with this mod. Make sure you're already subscribed to it to have the thumbnails." },
+
+            };
+        }
+
+        public void Unload()
+        {
+
         }
     }
 }
